364cc3ffd4b75a7cc73b2bb975dc43d66f6bdb13
leo-leao <leonardoleao486@gmail.com>
2022-05-11 22:29:00 -0300

    Correção do código de simulação da turbina JT3C-7

--- JT3C-7.py
+++ JT3C-7.py
@@ -7,7 +7,7 @@ table_a22 = pd.read_excel("table_a22.xlsx")
 
 def simulacao(table_a22, a, b, c, d, e):
     # Parâmetros da turbina JT3C-7
-    m_ar = 81.65                                            # [kg/s]
+    m_ar_iso = 81.65                                            # [kg/s]
     pr = 12.5
     impulso_max = 53500                                     # [N]
     impulso_cruzeiro = 15800                                # [N]
@@ -23,30 +23,37 @@ def simulacao(table_a22, a, b, c, d, e):
     """Caracteristicas ISO"""
     temp_iso = func.kelvin(15)                              # [K]
     p_iso = 101.325                                         # [kPa]
-    R_ar_iso = 0.816                                        # [kJ/kg*°C]
-    v_iso = temp_iso*R_ar_iso/p_iso                         # [m3/kg]
-
-    """Dados"""
-    pr_comp = 12.5
-    v_in = 260                                              # [m/s]
-    m_ar_in = 31.51                                         # [kg/s]
-    PCI = 42800                                             # [kJ/kg]
-    n_comp = c
-    n_exps = d
-    delta_p = a/100                                         # [kJ/kg*°C]
-    v = 2.116                                               # [m3/kg]
-    pho = 0.473                                             # [kg/m3]
-    phi_ar = 66.66                                          # [m3/s]
-    area_frontal = 0.26                                     # [m2]
-    diametro = 0.57                                         # [m]
-
-
+    R_ar_iso = 8.314/28.96                                        # [kJ/kg*°C]
+    v_iso = temp_iso*R_ar_iso/p_iso  
+    
     # Avião a jato voando a 260m/s e 9000 metros de altitude
     T_amb = func.kelvin(-43.15)
     p_amb = 31.2
     inter = func.getValues(table_a22, "T", T_amb)
     h_amb = inter["h"].values[0]  
-    pr_amb = inter["pr"].values[0] 
+    pr_amb = inter["pr"].values[0]
+
+    # Avião a jato voando a 260m/s e 7000 metros de altitude
+    T_amb = func.kelvin(-29.7915)
+    p_amb = 41.6948
+    inter = func.getValues(table_a22, "T", T_amb)
+    h_amb = inter["h"].values[0]  
+    pr_amb = inter["pr"].values[0]
+
+    """Dados"""
+    pr_comp = 12.5
+    v_in = 260                                              # [m/s]
+    R_ar = 8.314/28.97
+    v = R_ar*T_amb/p_amb                                    # [m3/kg]
+    m_ar_in = m_ar_iso*v_iso/v                              # [kg/s]
+    PCI = 42800                                             # [kJ/kg]
+    n_comp = c
+    n_exps = d
+    delta_p = a/100                                         # [kJ/kg*°C]
+    pho = 1/v                                               # [kg/m3]
+    phi_ar = m_ar_in*v                                      # [m3/s]
+    area_frontal = phi_ar/v_in                              # [m2]
+    diametro = (area_frontal*4/3.1418)**0.5                 # [m]
 
     # Estado 1:
     h_1 = h_amb+(v_in**2)/2/1000
@@ -132,7 +139,8 @@ def simulacao(table_a22, a, b, c, d, e):
     return l[-1]
 
 
-pdc, ec, ee = 2.5, 0.869, 0.856
+pdc, ec, ee = 2.1, 0.869, 0.854
+#pdc, ec, ee = 2.5, 0.87, 0.87
 simulacao(table_a22, pdc, 0, ec, ee, 0)
 
 """
